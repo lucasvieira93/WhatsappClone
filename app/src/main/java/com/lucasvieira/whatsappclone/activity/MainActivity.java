aa632d3c2d117db5d5f15bbf465f1b7b9ee90403
Lucas Vieira <lvieirastos@gmail.com>
2021-02-25 18:31:10 -0300

    Initial commit

---
+++ app/src/main/java/com/lucasvieira/whatsappclone/activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.lucasvieira.whatsappclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.lucasvieira.whatsappclone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getSupportActionBar().hide();
+
+//        referenciaFirebase.child("pontos").setValue(100);
+
+    }
+}
