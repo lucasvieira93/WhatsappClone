4324c00e8430f3a0d2dac5c5cfe34fe2a2aaf9e2
Lucas Vieira <lvieirastos@gmail.com>
2021-04-20 04:15:09 -0300

    Layout Chat

--- app/src/main/java/com/lucasvieira/whatsappclone/activity/ChatActivity.java
+++ app/src/main/java/com/lucasvieira/whatsappclone/activity/ChatActivity.java
@@ -17,18 +17,12 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 }
